5591a217c82c150a75d44875eeaa6bb13dee9b94
Ake <54357112+KhadaAke@users.noreply.github.com>
2023-03-09 17:46:05 +0200

    Update game_settings.py

--- game_settings.py
+++ game_settings.py
@@ -14,6 +14,7 @@ class Settings:
         self.screen_size = (1250, 660)
         self.screen_width = 1250
         self.screen_height = 660
+        # game background images
         self.bg_img = pygame.image.load('images/background/space.jpg')
         self.second_bg = pygame.image.load('images/background/space2.png')
         self.third_bg = pygame.image.load('images/background/space4.jpg')
@@ -22,15 +23,15 @@ class Settings:
         self.fire_sound = pygame.mixer.Sound('sounds/fire.wav')
 
         # Ships settings
-        self.max_ships = 5
-        self.ship_limit = 3
-        self.player_two_hp = 3
+        self.max_hp = 5
+        self.thunderbird_hp = 3
+        self.phoenix_hp = 3
 
         # Thunderbolt settings
-        self.thunder_bullet_count = 1
+        self.thunderbird_bullet_count = 1
 
         # Firebird settings
-        self.fire_bullet_count = 1
+        self.phoenix_bullet_count = 1
 
         # Alien settings
         self.alien_direction = 1
@@ -64,14 +65,14 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game"""
-        self.first_player_ship_speed = 3.5
-        self.second_player_ship_speed = 3.5
-        self.first_player_bullet_speed = 5.0
-        self.second_player_bullet_speed = 5.0
+        self.thunderbird_ship_speed = 3.5
+        self.phoenix_ship_speed = 3.5
+        self.thunderbird_bullet_speed = 5.0
+        self.phoenix_bullet_speed = 5.0
         self.alien_speed = 1.2
         self.alien_bullet_speed = 1.5
-        self.first_player_bullets_allowed = 1
-        self.second_player_bullets_allowed = 1
+        self.thunderbird_bullets_allowed = 1
+        self.phoenix_bullets_allowed = 1
 
         # Scoring
         self.alien_points = 1
@@ -82,8 +83,8 @@ class Settings:
             self.alien_speed += self.speedup_scale
             self.alien_bullet_speed += self.speedup_scale
         self.alien_points = int(self.alien_points + self.score_scale)
+        # bosses have more hp on medium or hard difficulty
         if self.speedup_scale == 0.5:
             self.boss_hp = 75
         elif self.speedup_scale == 0.7:
             self.boss_hp = 100
-
